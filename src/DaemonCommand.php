269b6779e7fa57ac190475c3b5afcb16fd3f331a
Alexey Samoylov <alexey.samoylov@gmail.com>
2016-07-25 13:30:53 +0700

    init

---
+++ src/DaemonCommand.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @author Alexey Samoylov <alexey.samoylov@gmail.com>
+ */
+namespace yarcode\yii2\daemon;
+
+use React\EventLoop\Factory;
+use React\EventLoop\LoopInterface;
+use yii\console\Controller;
+
+abstract class DaemonCommand extends Controller
+{
+    /** @var LoopInterface */
+    protected $loop;
+
+    public function init()
+    {
+        parent::init();
+        $this->loop = Factory::create();
+    }
+
+    public function actionDaemon()
+    {
+        $this->prepare();
+        $this->loop->run();
+    }
+
+    abstract public function prepare();
+}
